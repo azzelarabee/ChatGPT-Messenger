18081a22d3976dd6f806867560e621fa9e2e0c0a
azzelarabee <belghitatearabe1@gmail.com>
2023-02-24 09:46:48 +0100

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,19 @@ export default function RootLayout({
 }) {
     return (
         <html>
+            
             <head />
-            <body>{children}</body>
-             
+            <body>
+             <div className="flex">
+                {/* sidebar */}
+
+                {/* ClientProvider - Notification*/}
+
+                <div className="bg-[#343541] flex-1">{children}</div>
+
+
+             </div>
+            </body> 
         </html>
-    )
+    );
 }
