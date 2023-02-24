bdfc5bc0c554c002310fecd79e52de31382effe0
azzelarabee <belghitatearabe1@gmail.com>
2023-02-24 13:09:58 +0100

    7th commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ export default async function RootLayout({
     children: React.ReactNode
 }) {
     const session = await getServerSession(authOptions);
+
+    console.log(session);
     return (
         <html>
 
