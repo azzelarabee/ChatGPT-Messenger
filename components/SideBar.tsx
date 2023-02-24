4391594063e19e42bda4ec60ba2228f54315292c
azzelarabee <belghitatearabe1@gmail.com>
2023-02-24 14:31:52 +0100

    8th commit

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,9 @@
+'use client'
+import { useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function SideBar() {
+    const {data:session} = useSession();
   return (
     <div className="p-2 flex flex-col h-screen">
         <div className="flex-1">
@@ -11,6 +14,12 @@ function SideBar() {
                     {/* map through thre cartows*/}
                 </div>
             </div>
+            {session && (
+                <img src={session.user?.image!} alt="Profile pic" 
+                className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2
+                hover:opacity-50"
+                />
+            )}
         </div>
 );
 } 
