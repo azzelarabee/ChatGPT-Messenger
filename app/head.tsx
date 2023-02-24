baafaee6bccbf9ffd20273723e41a5c88e5ecc10
azzelarabee <belghitatearabe1@gmail.com>
2023-02-24 08:57:55 +0100

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+        <title>ChatGPT Clone</title>
+        <meta content="width=device-width,
+        initialscale=1" name="viewport" />
+        <link rel="icon" href="/favion.ico" />
+        </>
+    );
+}
