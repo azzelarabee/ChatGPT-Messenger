f90f1e0a379b717022fe80304a0cfbb670263681
azzelarabee <belghitatearabe1@gmail.com>
2023-02-25 18:44:39 +0100

    last

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -40,7 +40,7 @@ function ChatRow({id}:Props) {
     >
         <ChatBubbleLeftIcon className="h-5 w-5" />
         <p className="flex-1 hidden md:inline-flex truncate">
-            {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
+            {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"} 
         </p>
         <TrashIcon onClick={removeChat} className="h-5 w-5 text-gray-700 hover:text-red-700" />
     </Link>
